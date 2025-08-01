85af715770e454dc826e43cbfe3188be8fa1d26e
Piotr Sobieszczański <piotr.sobieszczanski@gmail.com>
2025-08-01 08:35:47 +0200

    Refactor VPC module configuration for clarity and consistency

--- main.tf
+++ main.tf
@@ -23,13 +23,22 @@ locals {
 ################################################################################
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = ">= 6.0.0"
 
   name = local.name
   cidr = local.vpc_cidr
 
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  manage_default_network_acl    = false
+  manage_default_route_table    = false
+  manage_default_security_group = false
+
   azs             = local.azs
   private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k + 4)]
 
   tags = local.tags
 }
