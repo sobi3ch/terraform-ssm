c1beb719e2119117dce1f5fd427ca8e7e8573936
Piotr Sobieszczański <piotr.sobieszczanski@gmail.com>
2025-08-02 11:12:23 +0200

    Update README and main.tf to remove SSH instructions; streamline SSM access guidance

--- README.md
+++ README.md
@@ -9,13 +9,8 @@ This Terraform configuration sets up an EC2 instance with SSM access, banning SS
 1. `terrafrom apply`
 
 ### Connect to instance via Session Manager over CLI
-SSH to public instance. Get SSH key
-```bash
-terraform output -raw ssm_demo_private_key_pem > ssm-demo-key.pem
-chmod 600 ssm-demo-key.pem
-```
 
-Get instance ID from the output
+Test connection with Session Manager 
 ```bash
-aws ssm start-session --target <instance-id>
+aws ssm start-session --target $(terraform output -raw ec2_instance_id_private)
 ```
--- main.tf
+++ main.tf
@@ -12,10 +12,7 @@ module "vpc" {
   name = local.name
   cidr = local.vpc_cidr
 
-  enable_nat_gateway      = true
-  single_nat_gateway      = true
-  enable_dns_hostnames    = true
-  map_public_ip_on_launch = true
+  enable_dns_hostnames = true
 
   manage_default_network_acl    = false
   manage_default_route_table    = false
@@ -23,7 +20,6 @@ module "vpc" {
 
   azs             = local.azs
   private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
-  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k + 4)]
 
   tags = local.tags
 }
